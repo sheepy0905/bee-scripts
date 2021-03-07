65c10d3a582b197ff393639958df5ea2e5c78a3c
Tony C <cyy8913@gmail.com>
2021-03-06 20:08:22 -0700

    Update printpassword.sh

--- printpassword.sh
+++ printpassword.sh
@@ -1,4 +1,4 @@
-!#/bin/bash
+#1/usr/bin/env sh
 
 cat /var/lib/docker/volumes/bee_clef-1/_data/password && echo
 cat /var/lib/docker/volumes/bee_clef-1/_data/keystore/$(ls -1 /var/lib/docker/volumes/bee_clef-1/_data/keystore/ | head -1) && echo
